31313f71fd9128b0c57c429c30aaef10a6df9550
Jieun Choi <cje172@naver.com>
2023-04-14 15:25:33 +0900

    design: #2 내 정보 입력 - 서비스 이용 유형 화면 UI 구현

--- app/src/main/java/com/swu/caresheep/StartActivity.kt
+++ app/src/main/java/com/swu/caresheep/StartActivity.kt
@@ -1,16 +1,28 @@
 package com.swu.caresheep
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.swu.caresheep.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
 
     private lateinit var splashScreen : SplashScreen
+    private lateinit var binding: ActivityStartBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         splashScreen = installSplashScreen()
-        setContentView(R.layout.activity_start)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 임시 이동
+        binding.btnGoogleSignIn.setOnClickListener{
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
