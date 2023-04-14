132ba8a50c36b993ea66376f1084988a9ed0eb55
Jieun Choi <cje172@naver.com>
2023-04-14 23:28:05 +0900

    design: #3 보호자 화면 Bottom Navigation 구현

---
+++ app/src/main/java/com/swu/caresheep/ui/guardian/GuardianMyPageFragment.kt
@@ -0,0 +1,19 @@
+package com.swu.caresheep.ui.guardian
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.swu.caresheep.R
+
+class GuardianMyPageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_guardian_my_page, container, false)
+    }
+}
