f04ac8c3b5dc0e88f1dc032b3d74ff6cd8589569
Jieun Choi <cje172@naver.com>
2023-04-15 02:04:54 +0900

    design: #3 보호자 홈 화면 UI 구현

--- app/src/main/java/com/swu/caresheep/ui/guardian/GuardianHomeFragment.kt
+++ app/src/main/java/com/swu/caresheep/ui/guardian/GuardianHomeFragment.kt
@@ -13,6 +13,7 @@ class GuardianHomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_guardian_home, container, false)
     }
