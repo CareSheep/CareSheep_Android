9def148cbfdf23de535bb32594553084004ecc16
YejinHong0414 <hongyejin414@gmail.com>
2023-04-20 15:28:03 +0900

    design:보호자 환자 루틴 설정 UI구현

---
+++ app/src/main/java/com/swu/caresheep/ui/guardian/GuardianElderReportActivity.kt
@@ -0,0 +1,12 @@
+package com.swu.caresheep.ui.guardian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.swu.caresheep.R
+
+class GuardianElderReportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guardian_elder_report)
+    }
+}
