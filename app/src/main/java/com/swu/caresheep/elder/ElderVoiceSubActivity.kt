9f167e977617fa5920779c8e9c771c75584e6971
whitecastle20 <whitecastle20@github.com>
2023-05-03 11:25:41 +0900

    feat : 어르신 - STT 기능 구현

--- app/src/main/java/com/swu/caresheep/elder/ElderVoiceSubActivity.kt
+++ app/src/main/java/com/swu/caresheep/elder/ElderVoiceSubActivity.kt
@@ -1,12 +1,48 @@
 package com.swu.caresheep.elder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.widget.Toast
 import com.swu.caresheep.R
+import kotlinx.android.synthetic.main.activity_elder_voice_sub.*
 
 class ElderVoiceSubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_elder_voice_sub)
+
+        button2.setOnClickListener {
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+                putExtra(
+                    RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+                )
+                putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR")
+            }
+
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT)
+            } else {
+                Toast.makeText(this, "음성 인식을 지원하지 않는 기기입니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT && resultCode == RESULT_OK) {
+            val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+
+            if (!result.isNullOrEmpty()) {
+                voice_question.text = result[0]
+            }
+        }
+    }
+
+    companion object {
+        private const val REQUEST_CODE_SPEECH_INPUT = 100
     }
 }
