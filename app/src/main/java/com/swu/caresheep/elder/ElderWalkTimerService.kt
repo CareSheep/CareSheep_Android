0e3eb8e85a074e4d7b74fb83f2e38676c62a247d
whitecastle20 <whitecastle20@github.com>
2023-05-11 20:29:30 +0900

    chore : 파이어베이스 URL buildConfig & 만보기 에러 주석 처리

--- app/src/main/java/com/swu/caresheep/elder/ElderWalkTimerService.kt
+++ app/src/main/java/com/swu/caresheep/elder/ElderWalkTimerService.kt
@@ -6,38 +6,38 @@ import java.util.*
 
 //만보기 스탑워치를 위한 코드
 
-class ElderWalkTimerService : Service() {
-    override fun onBind(p0: Intent?): IBinder? = null
-
-    private val timer = Timer()
-
-    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int
-    {
-        val time = intent.getDoubleExtra(TIME_EXTRA, 0.0)
-        timer.scheduleAtFixedRate(TimeTask(time), 0, 1000)
-        return START_NOT_STICKY
-    }
-
-    override fun onDestroy()
-    {
-        timer.cancel()
-        super.onDestroy()
-    }
-
-    private inner class TimeTask(private var time: Double) : TimerTask()
-    {
-        override fun run()
-        {
-            val intent = Intent(TIMER_UPDATED)
-            time++
-            intent.putExtra(TIME_EXTRA, time)
-            sendBroadcast(intent)
-        }
-    }
-
-    companion object
-    {
-        const val TIMER_UPDATED = "timerUpdated"
-        const val TIME_EXTRA = "timeExtra"
-    }
-}
+//class ElderWalkTimerService : Service() {
+//    override fun onBind(p0: Intent?): IBinder? = null
+//
+//    private val timer = Timer()
+//
+//    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int
+//    {
+//        val time = intent.getDoubleExtra(TIME_EXTRA, 0.0)
+//        timer.scheduleAtFixedRate(TimeTask(time), 0, 1000)
+//        return START_NOT_STICKY
+//    }
+//
+//    override fun onDestroy()
+//    {
+//        timer.cancel()
+//        super.onDestroy()
+//    }
+//
+//    private inner class TimeTask(private var time: Double) : TimerTask()
+//    {
+//        override fun run()
+//        {
+//            val intent = Intent(TIMER_UPDATED)
+//            time++
+//            intent.putExtra(TIME_EXTRA, time)
+//            sendBroadcast(intent)
+//        }
+//    }
+//
+//    companion object
+//    {
+//        const val TIMER_UPDATED = "timerUpdated"
+//        const val TIME_EXTRA = "timeExtra"
+//    }
+//}
