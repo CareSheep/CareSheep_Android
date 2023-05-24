3c0d565f598324a179380bbf239523b9fe88bf75
whitecastle20 <whitecastle20@github.com>
2023-05-25 05:11:49 +0900

    chore : 예진님 작업하시던 곳에 합침

--- app/src/main/java/com/swu/caresheep/ui/guardian/medicine/GuardianSetMedicineTimeActivity4.kt
+++ app/src/main/java/com/swu/caresheep/ui/guardian/medicine/GuardianSetMedicineTimeActivity4.kt
@@ -1,5 +1,8 @@
 package com.swu.caresheep.ui.guardian.medicine
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -11,11 +14,13 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.swu.caresheep.MedicineTime
 import com.swu.caresheep.R
+import com.swu.caresheep.elder.AlarmReceiver
 import com.swu.caresheep.ui.guardian.GuardianActivity
 import kotlinx.android.synthetic.main.activity_guardian_set_medicine_time4.fourthMedcineButton
 import kotlinx.android.synthetic.main.activity_guardian_set_medicine_time4.fourththirdMedcinetimePicker
-import java.util.HashMap
+import java.util.*
 
 var result4 : String = ""
 
@@ -80,6 +85,82 @@ class GuardianSetMedicineTimeActivity4 : AppCompatActivity() {
                 }
             })
         }
+
+        // Firebase Realtime Database에서 데이터 가져오기
+        val database =
+            FirebaseDatabase.getInstance("https://caresheep-dcb96-default-rtdb.asia-southeast1.firebasedatabase.app/")
+        val reference = database.getReference("MedicineTime")
+        reference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (childSnapshot in dataSnapshot.children) {
+                    val medicineData = childSnapshot.getValue(MedicineTime::class.java)
+
+
+                    // time 필드 String으로 변환
+                    val timeString = medicineData!!.time
+
+                    Log.d("Firebase", "시간 값: $timeString")
+
+                    // 시간 : 분 형태로 돼있어서 split으로 분리시킨 후 int로 변환
+                    val timeParts = timeString.split(":")
+                    if (timeParts.size == 2) {
+                        val hour = timeParts[0].toIntOrNull()
+                        val minute = timeParts[1].toIntOrNull()
+                        // 해당 시간에 알람 설정
+                        if (hour != null && minute != null) {
+                            val calendar = Calendar.getInstance()
+                            calendar.set(Calendar.HOUR_OF_DAY, hour)
+                            calendar.set(Calendar.MINUTE, minute)
+                            calendar.set(Calendar.SECOND, 0)
+
+                            // 현재 시간보다 이전이면 다음 날로 설정
+                            if (calendar.before(Calendar.getInstance())) {
+                                calendar.add(Calendar.DATE, 1)
+                            }
+
+                            val alarmIntent = Intent(this@GuardianSetMedicineTimeActivity4, AlarmReceiver::class.java)
+                            val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                            alarmIntent.action = AlarmReceiver.ACTION_RESTART_SERVICE
+                            val alarmCallPendingIntent = PendingIntent.getBroadcast(
+                                this@GuardianSetMedicineTimeActivity4,
+                                0,
+                                alarmIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT
+                            )
+
+
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                alarmManager.setExactAndAllowWhileIdle(
+                                    AlarmManager.RTC_WAKEUP,
+                                    calendar.timeInMillis,
+                                    alarmCallPendingIntent
+                                )
+                            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                                alarmManager.setExact(
+                                    AlarmManager.RTC_WAKEUP,
+                                    calendar.timeInMillis,
+                                    alarmCallPendingIntent
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // 데이터 가져오기가 실패한 경우 처리할 내용
+                val exception = databaseError.toException()
+                Log.e("FirebaseDatabase", "Failed to read value: ${exception.message}", exception)
+
+            }
+        })
+
+
+
+
+
+
+
     }
 
     private fun nextPage() {
@@ -119,4 +200,6 @@ class GuardianSetMedicineTimeActivity4 : AppCompatActivity() {
         Log.d("T",result4)
 
     }
+
+
 }
