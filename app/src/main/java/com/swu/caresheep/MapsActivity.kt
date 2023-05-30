915e79d418fbaf836b1f83a7d545cc477900802e
whitecastle20 <whitecastle20@github.com>
2023-05-30 23:25:44 +0900

    feat : 현재위치 db에 저장

--- app/src/main/java/com/swu/caresheep/MapsActivity.kt
+++ app/src/main/java/com/swu/caresheep/MapsActivity.kt
@@ -7,6 +7,8 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.FragmentManager
+import com.google.android.gms.location.LocationServices
+import com.google.firebase.database.FirebaseDatabase
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
@@ -14,11 +16,14 @@ import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private val TAG = "MainActivity"
 
+    // 권한
     private val PERMISSION_REQUEST_CODE = 100
     private val PERMISSIONS = arrayOf<String>(
         Manifest.permission.ACCESS_FINE_LOCATION,
@@ -51,8 +56,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mNaverMap!!.locationSource = mLocationSource
 
         ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_REQUEST_CODE)
+
+
+
     }
 
+    // 권한
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
@@ -63,6 +72,59 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         if (requestCode == PERMISSION_REQUEST_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 mNaverMap!!.locationTrackingMode = LocationTrackingMode.Follow
+
+                // 위치 정보를 가져온 후 Firebase에 저장
+                val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
+
+                val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+
+                fusedLocationClient.lastLocation
+                    .addOnSuccessListener { location ->
+                        // 위치 정보를 성공적으로 가져왔을 때
+                        if (location != null) {
+                            val database =
+                                FirebaseDatabase.getInstance("https://caresheep-dcb96-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                                .getReference("Location")
+
+                            // Voice의 각 필드에 넣기
+                            val location = Location(
+                                latitude = location.latitude.toString(),
+                                longitude = location.longitude.toString(),
+                                location_date = timeStamp,
+                                user_id = 1,
+
+                            )
+                            database.child(timeStamp).setValue(location)   // 데이터가 계속 쌓이도록(timeStamp가 참조 꼬리로 쌓이도록)
+                                //업로드 성공했는지 확인해보려고
+                                .addOnSuccessListener {
+                                    Log.d("Firebase", "데이터 업로드 성공")
+                                }
+                                .addOnFailureListener { exception ->
+                                    Log.e("Firebase", "데이터 업로드 실패: ${exception.message}", exception)
+                                }
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        // 위치 정보를 가져오는 데 실패했을 때
+                        Log.e(TAG, "Error getting location", exception)
+                    }
             }
         }
     }
