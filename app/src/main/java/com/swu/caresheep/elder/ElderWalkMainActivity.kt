d9f674e06bf04d348b8830e87accda153485d19d
YejinHong0414 <hongyejin414@gmail.com>
2023-06-04 16:43:54 +0900

    fix: 만보기 UI오류 수정

--- app/src/main/java/com/swu/caresheep/elder/ElderWalkMainActivity.kt
+++ app/src/main/java/com/swu/caresheep/elder/ElderWalkMainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.swu.caresheep.R
+import com.swu.caresheep.ui.elder.main.ElderActivity
 import kotlinx.android.synthetic.main.activity_elder_walk_main.walk_no
 import kotlinx.android.synthetic.main.activity_elder_walk_main.walk_yes
 
@@ -13,14 +14,16 @@ class ElderWalkMainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_elder_walk_main)
 
-        // '네' 버튼 => 녹음 화면
+        // '네' 버튼 => 만보기 화면
         walk_yes.setOnClickListener {
             val intent = Intent(this, ElderWalkActivity::class.java)
             startActivity(intent)
         }
         // '아니오' 버튼 => 홈 화면
         walk_no.setOnClickListener {
-            finish()
+            //finish()
+            val intent = Intent(this, ElderActivity::class.java)
+            startActivity(intent)
         }
     }
 }
