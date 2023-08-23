afc2ac55431496b11e089d82baf2c118cca25bc0
최지은 <cje172@naver.com>
2023-08-23 18:21:54 +0900

    Feat: #81 어르신 위치 추적 시, 포그라운드화 구현
    앱을 꺼도 보호자에게 위치를 제공할 수 있도록 어르신 화면에서 지도 화면에 진입할 시 포그라운드 서비스가 실행되고 서비스를 종료하지 않는 한, 실시간 위치 추적을 계속 진행한다.

--- app/src/main/java/com/swu/caresheep/ElderMapsActivity.kt
+++ app/src/main/java/com/swu/caresheep/ElderMapsActivity.kt
@@ -1,242 +1,179 @@
 package com.swu.caresheep
 
-import android.Manifest
-import android.location.Location
-import android.annotation.SuppressLint
-import android.content.pm.PackageManager
+import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.os.Looper
-import android.util.Log
+import android.provider.Settings
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.FragmentManager
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.firebase.annotations.concurrent.UiThread
-import com.google.firebase.database.FirebaseDatabase
-import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.*
-import com.naver.maps.map.overlay.Marker
-import com.naver.maps.map.util.FusedLocationSource
-import com.swu.caresheep.BuildConfig.DB_URL
-import java.text.SimpleDateFormat
-import java.util.*
-
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.swu.caresheep.databinding.ActivityElderMapsBinding
+import com.swu.caresheep.utils.*
 
 class ElderMapsActivity : AppCompatActivity(), OnMapReadyCallback {
-    private val TAG = "MainActivity"
 
-    // 권한
-    private val PERMISSION_REQUEST_CODE = 100
-//    private val PERMISSIONS = arrayOf<String>(
-//        Manifest.permission.ACCESS_FINE_LOCATION,
-//        Manifest.permission.ACCESS_COARSE_LOCATION
-//    )
-    var permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION,
-                            android.Manifest.permission.ACCESS_COARSE_LOCATION)
-    private var mLocationSource: FusedLocationSource? = null
-    private var mNaverMap: NaverMap? = null
+    private lateinit var binding: ActivityElderMapsBinding
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var mNaverMap: NaverMap
+
+    companion object {
+        // 앱 설정 화면으로 이동
+        fun moveAppSettings(elderMapsActivity: ElderMapsActivity, requestCode: Int) {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            val uri = Uri.fromParts("package", elderMapsActivity.packageName, null)
+            intent.data = uri
+
+            when (requestCode) {
+                FOREGROUND_LOCATION_PERMISSION_REQUEST_CODE -> elderMapsActivity.moveLocationSettingsLauncher.launch(
+                    intent
+                )
+                BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE -> elderMapsActivity.moveLocationSettingsLauncher.launch(
+                    intent
+                )
+                NOTIFICATION_PERMISSION_REQUEST_CODE -> elderMapsActivity.moveNotificationSettingsLauncher.launch(
+                    intent
+                )
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_elder_maps)
+        binding = ActivityElderMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(this)  // 자동으로 GPS 받아오기
 
-//        val fm: FragmentManager = supportFragmentManager
-//        var mapFragment: MapFragment? = fm.findFragmentById(R.id.map) as MapFragment?
-//        if (mapFragment == null) {
-//            mapFragment = MapFragment.newInstance()
-//            fm.beginTransaction().add(R.id.map, mapFragment).commit()
-//        }
-//        // callback으로 onMapReady에 연결
-//        mapFragment!!.getMapAsync(this)
-//
-//        mLocationSource = FusedLocationSource(this, PERMISSION_REQUEST_CODE)
-//
+        startMapProcess()
 
-        if(isPermitted())
-            startProcess()
-        else
-            ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQUEST_CODE)
-
+        // 백그라운드 위치 권한 요청
+        LocationUtil.initBackgroundLocationUtil(this)
     }
 
-    // 권한 확인
-    fun isPermitted() : Boolean {
-        for(perm in permissions) {
-            if (ContextCompat.checkSelfPermission(this, perm) != PackageManager.PERMISSION_GRANTED) {
-                return false
-            }
-        }
-        return true
-    }
 
-    // 권한이 있을 때 callback으로 onMapReady에 연결해서 startProcess 함수 실행
-    fun startProcess() {
+    // 지도 연결 및 초기화
+    private fun startMapProcess() {
         val fm = supportFragmentManager
-        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+        val mapFragment = fm.findFragmentById(R.id.fcv_map) as MapFragment?
             ?: MapFragment.newInstance().also {
-                fm.beginTransaction().add(R.id.map, it).commit()
+                fm.beginTransaction().add(R.id.fcv_map, it).commit()
             }
+
         mapFragment.getMapAsync(this)
     }
 
-    @UiThread
-    // GPS 가져오기
-    override fun onMapReady(naverMap: NaverMap) {
-        Log.d(TAG, "onMapReady")
-
-//        mNaverMap = naverMap
-//        mNaverMap!!.locationSource = mLocationSource
-//
-//        ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_REQUEST_CODE)
-
-        val cameraPosition = CameraPosition(
-            LatLng(37.5666102, 126.9783881), // 위치 지정(현재 GPS로 이동할 것이라 임의로)
-            16.0 // 줌 레벨
-            )
-        naverMap.cameraPosition = cameraPosition
-        this.mNaverMap = naverMap
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this) // 자동으로 GPS 받아오기
-        setUpdateLocationListener() // 내 위치 가져오기
-    }
-    lateinit var fusedLocationProviderClient : FusedLocationProviderClient
-    lateinit var locationCallBack : LocationCallback // GPS 응답 값 가져오기
-
-
-    // 좌표계 주기적 갱신
-    @SuppressLint("MissingPermission")
-    fun setUpdateLocationListener() {
-        val locationRequest = LocationRequest.create()
-        locationRequest.run {
-            priority = LocationRequest.PRIORITY_HIGH_ACCURACY // 높은 정확도
-            interval = 1000 // 1초에 한번씩 GPS 요청
-        }
-        locationCallBack = object: LocationCallback() {
-            override fun onLocationResult(locationResult: LocationResult?) {
-                locationResult?: return
-                for ((i, location) in locationResult.locations.withIndex()) {
-                    Log.d("location: ", "${location.latitude}, ${location.longitude}")
-                    setLastLocation(location)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == FOREGROUND_LOCATION_PERMISSION_REQUEST_CODE) {
+            if (!LocationUtil.hasForegroundLocationPermission(this)) {
+                LocationUtil.initForegroundLocationUtil(this)
+            } else {
+                NotificationManager.initNotificationManager(this)
+            }
+        } else if (requestCode == BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE) {
+            if (LocationUtil.hasBackgroundLocationPermission(this)) {
+                NotificationManager.initNotificationManager(this)
+            } else {
+                // 설정 이동
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("서비스 이용 알림").setCancelable(false)
+                builder.setMessage("앱을 사용하기 위해서는 위치 권한이 필요합니다. 설정으로 이동하여 권한을 항상 허용해주세요.")
+                builder.setPositiveButton("설정으로 이동") { _, _ ->
+                    moveAppSettings(this, requestCode)
+                }
+                builder.show()
+            }
+        } else if (requestCode == NOTIFICATION_PERMISSION_REQUEST_CODE) {
+            if (NotificationManager.hasNotificationPermission(this)) {
+                // 권한이 허용된 경우
+                if (LocationUtil.hasAllPermissions(this)) {
+                    // 포그라운드 서비스 시작
+                    val serviceIntent = Intent(this, GpsForegroundService::class.java)
+                    ContextCompat.startForegroundService(this, serviceIntent)
+                } else {
+                    val builder = AlertDialog.Builder(this)
+                    builder.setTitle("서비스 이용 알림").setCancelable(false)
+                    builder.setMessage("앱을 사용하기 위해서는 위치 권한이 필요합니다. 설정으로 이동하여 권한을 항상 허용해주세요.")
+                    builder.setPositiveButton("설정으로 이동") { _, _ ->
+                        moveAppSettings(this, FOREGROUND_LOCATION_PERMISSION_REQUEST_CODE)
+                    }
+                    builder.show()
+                }
+            } else {
+                // 권한이 거부된 경우
+                // 포그라운드 서비스가 진행 중이면 종료
+                if (GpsForegroundService.isServiceRunning) {
+                    val serviceIntent = Intent(this, GpsForegroundService::class.java)
+                    stopService(serviceIntent)
+                }
+                if (LocationUtil.hasAllPermissions(this)) {
+                    val builder = AlertDialog.Builder(this)
+                    builder.setTitle("서비스 이용 알림").setCancelable(false)
+                    builder.setMessage("앱을 사용하기 위해서는 알림 권한이 필요합니다. 설정으로 이동하여 권한을 허용해주세요.")
+                    builder.setPositiveButton("설정으로 이동") { _, _ ->
+                        moveAppSettings(this, requestCode)
+                    }
+                    builder.show()
+                } else {
+                    val builder = AlertDialog.Builder(this)
+                    builder.setTitle("서비스 이용 알림").setCancelable(false)
+                    builder.setMessage("앱을 사용하기 위해서는 위치, 알림 권한이 필요합니다. 설정으로 이동하여 권한을 항상 허용해주세요.")
+                    builder.setPositiveButton("설정으로 이동") { _, _ ->
+                        moveAppSettings(this, requestCode)
+                    }
+                    builder.show()
                 }
             }
+
         }
-        //location 요청 함수 호출 (locationRequest, locationCallback)
-        fusedLocationProviderClient.requestLocationUpdates(
-            locationRequest,
-            locationCallBack,
-            Looper.myLooper()
-        )
     }
 
-    //setUpdateLocationListner 함수에서 받아온 GPS 좌표로 현재 위치를 마커로 출력
-    fun setLastLocation(location: Location) {
-        val myLocation = LatLng(location.latitude, location.longitude)
-        val marker = Marker()
-        marker.position = myLocation
+    private val moveLocationSettingsLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            // 설정 화면에서 돌아왔을 때의 처리
+            LocationUtil.initBackgroundLocationUtil(this)
 
-        marker.map = mNaverMap
-        //마커
-        val cameraUpdate = CameraUpdate.scrollTo(myLocation)
-        mNaverMap?.moveCamera(cameraUpdate)
-        mNaverMap?.maxZoom = 18.0
-        mNaverMap?.minZoom = 5.0
-
-        // 파이어베이스 DB에 현재 위치 저장
-        val database = FirebaseDatabase.getInstance(DB_URL).getReference("Location")
-
-        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        // Location의 각 필드에 넣기
-        val locationData = Location(
-            latitude = location.latitude.toString(),
-            longitude  = location.longitude.toString(),
-            location_date = timeStamp,
-            user_id = 1
-        )
-        database.child(timeStamp).setValue(location)   // 데이터가 계속 쌓이도록(timeStamp가 참조 꼬리로 쌓이도록)
-            //업로드 성공했는지 확인해보려고
-            .addOnSuccessListener {
-                Log.d("Firebase", "데이터 업로드 성공")
+            if (LocationUtil.hasAllPermissions(this) && NotificationManager.hasNotificationPermission(
+                    this
+                ) && !GpsForegroundService.isServiceRunning
+            ) {
+                // 포그라운드 서비스 시작
+                LocationUtil.startGpsForegroundService()
             }
-            .addOnFailureListener { exception ->
-                Log.e("Firebase", "데이터 업로드 실패: ${exception.message}", exception)
+        }
+
+    private val moveNotificationSettingsLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            // 설정 화면에서 돌아왔을 때의 처리
+            NotificationManager.initNotificationManager(this)
+
+            if (LocationUtil.hasAllPermissions(this) && NotificationManager.hasNotificationPermission(
+                    this
+                ) && !GpsForegroundService.isServiceRunning
+            ) {
+                // 포그라운드 서비스 재시작
+                LocationUtil.startGpsForegroundService()
             }
+        }
+
+
+    @UiThread
+    override fun onMapReady(naverMap: NaverMap) {
+        this.mNaverMap = naverMap
+        LocationUtil.setMap(this, mNaverMap)
+
     }
-
-
-
-//    // 권한
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<String>,
-//        grantResults: IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//
-//        if (requestCode == PERMISSION_REQUEST_CODE) {
-//            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                mNaverMap!!.locationTrackingMode = LocationTrackingMode.Follow
-//
-//                // 위치 정보를 가져온 후 Firebase에 저장
-//                val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-//                if (ActivityCompat.checkSelfPermission(
-//                        this,
-//                        Manifest.permission.ACCESS_FINE_LOCATION
-//                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-//                        this,
-//                        Manifest.permission.ACCESS_COARSE_LOCATION
-//                    ) != PackageManager.PERMISSION_GRANTED
-//                ) {
-//                    // TODO: Consider calling
-//                    //    ActivityCompat#requestPermissions
-//                    // here to request the missing permissions, and then overriding
-//                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//                    //                                          int[] grantResults)
-//                    // to handle the case where the user grants the permission. See the documentation
-//                    // for ActivityCompat#requestPermissions for more details.
-//                    return
-//                }
-//
-//                val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-//
-//                fusedLocationClient.lastLocation
-//                    .addOnSuccessListener { location ->
-//                        // 위치 정보를 성공적으로 가져왔을 때
-//                        if (location != null) {
-//                            val database =
-//                                FirebaseDatabase.getInstance(DB_URL)
-//                                    .getReference("Location")
-//
-//                            // Voice의 각 필드에 넣기
-//                            val location = Location(
-//                                latitude = location.latitude.toString(),
-//                                longitude = location.longitude.toString(),
-//                                location_date = timeStamp,
-//                                user_id = 1,
-//
-//                                )
-//                            database.child(timeStamp).setValue(location)   // 데이터가 계속 쌓이도록(timeStamp가 참조 꼬리로 쌓이도록)
-//                                //업로드 성공했는지 확인해보려고
-//                                .addOnSuccessListener {
-//                                    Log.d("Firebase", "데이터 업로드 성공")
-//                                }
-//                                .addOnFailureListener { exception ->
-//                                    Log.e("Firebase", "데이터 업로드 실패: ${exception.message}", exception)
-//                                }
-//                        }
-//                    }
-//                    .addOnFailureListener { exception ->
-//                        // 위치 정보를 가져오는 데 실패했을 때
-//                        Log.e(TAG, "Error getting location", exception)
-//                    }
-//            }
-//        }
-//    }
-
 
 }
--- app/src/main/java/com/swu/caresheep/GuardianMapsActivity.kt
+++ app/src/main/java/com/swu/caresheep/GuardianMapsActivity.kt
@@ -1,12 +1,9 @@
 package com.swu.caresheep
 
-import android.Manifest
-import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.core.app.ActivityCompat
-import androidx.fragment.app.FragmentManager
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -14,91 +11,79 @@ import com.google.firebase.database.ValueEventListener
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
 import com.naver.maps.map.overlay.Marker
-import com.naver.maps.map.util.FusedLocationSource
 import com.swu.caresheep.BuildConfig.DB_URL
+import com.swu.caresheep.databinding.ActivityGuardianMapsBinding
+import com.swu.caresheep.ui.start.user_id
 
 class GuardianMapsActivity : AppCompatActivity(), OnMapReadyCallback {
-    private val TAG = "MainActivity"
 
-    // 권한
-    private val PERMISSION_REQUEST_CODE = 100
-    private val PERMISSIONS = arrayOf<String>(
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.ACCESS_COARSE_LOCATION
-    )
-    private var mLocationSource: FusedLocationSource? = null
+    private lateinit var binding: ActivityGuardianMapsBinding
+
     private var mNaverMap: NaverMap? = null
-
+    private var locationMarker: Marker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guardian_maps)
+        binding = ActivityGuardianMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val fm: FragmentManager = supportFragmentManager
-        var mapFragment: MapFragment? = fm.findFragmentById(R.id.guardian_map) as MapFragment?
-        if (mapFragment == null) {
-            mapFragment = MapFragment.newInstance()
-            fm.beginTransaction().add(R.id.guardian_map, mapFragment).commit()
-        }
+        startMapProcess()
+    }
 
-        mapFragment!!.getMapAsync(this)
+    // 지도 연결 및 초기화
+    private fun startMapProcess() {
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.fcv_map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.fcv_map, it).commit()
+            }
 
-        mLocationSource = FusedLocationSource(this, PERMISSION_REQUEST_CODE)
+        mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(naverMap: NaverMap) {
-        Log.d(TAG, "onMapReady")
-
         mNaverMap = naverMap
 
-        // 파이어베이스에서 위치 정보 가져오기
-        val database =
-            FirebaseDatabase.getInstance(DB_URL)
+        // DB에서 위치 정보 가져오기
+        val database = FirebaseDatabase.getInstance(DB_URL)
         val reference = database.getReference("Location")
-        val query = reference.orderByChild("timestamp").limitToLast(1) //맨 위 하나
+        val query = reference.orderByChild("user_id").equalTo(user_id.toDouble())
 
         query.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // 가장 최근에 저장된 위치 정보 가져오기
+                // 위치 정보 조회
                 val latestLocationSnapshot = dataSnapshot.children.lastOrNull()
                 val locationData = latestLocationSnapshot?.getValue(Location::class.java)
 
-                if (locationData != null) {
-                    // 위치 정보 화면에 표시
-                    //카메라 이동
+                if (locationData != null) {  // 위치 정보가 있는 경우
                     val latitude = locationData.latitude.toDouble()
                     val longitude = locationData.longitude.toDouble()
+
+                    if (locationMarker == null) {
+                        // 처음에 마커 생성
+                        val marker = Marker()
+                        marker.position = LatLng(latitude, longitude)
+                        marker.map = mNaverMap
+                        locationMarker = marker
+                    } else {
+                        // 이미 생성된 마커 위치 업데이트
+                        locationMarker?.position = LatLng(latitude, longitude)
+                    }
+
+                    // 카메라 이동
                     val cameraUpdate = CameraUpdate.scrollTo(LatLng(latitude, longitude))
                     naverMap.moveCamera(cameraUpdate)
-
-                    // 마커 추가
-                    val marker = Marker()
-                    marker.position = LatLng(latitude, longitude)
-                    marker.map = naverMap
+                } else {  // 위치 정보가 없는 경우
+                    Snackbar.make(binding.root, "어르신의 위치를 추적하는 중이 아닙니다.", Snackbar.LENGTH_SHORT).show()
                 }
-
             }
-            override fun onCancelled(databaseError: DatabaseError) {
+
+            override fun onCancelled(error: DatabaseError) {
                 // 데이터 가져오기가 실패한 경우 처리할 내용
-                val exception = databaseError.toException()
-                Log.e("FirebaseDatabase", "Failed to read value: ${exception.message}", exception)
+                Log.e("[위치추적] - 보호자", "Database error: $error")
             }
         })
-        ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_REQUEST_CODE)
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                mNaverMap!!.locationTrackingMode = LocationTrackingMode.Follow
-            }
-        }
-    }
 }
 
