03dc89e7b45824db28f1959d2e6c9cf1d999e2b9
최지은 <cje172@naver.com>
2023-11-24 03:31:28 +0900

    style: 루틴 추천 Activity명, xml id 수정

--- app/src/main/java/com/swu/caresheep/guardian/report/GuardianElderWeekReportDetailActivity.kt
+++ app/src/main/java/com/swu/caresheep/guardian/report/GuardianElderWeekReportDetailActivity.kt
@@ -3,20 +3,20 @@ package com.swu.caresheep.guardian.report
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.swu.caresheep.R
-import com.swu.caresheep.databinding.ActivityGuardianElderWeekReportMealBinding
+import com.swu.caresheep.databinding.ActivityGuardianElderWeekReportDetailBinding
 import com.swu.caresheep.utils.dialog.InputDialog
 
-class GuardianElderWeekReportMealActivity : AppCompatActivity() {
+class GuardianElderWeekReportDetailActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityGuardianElderWeekReportMealBinding
+    private lateinit var binding: ActivityGuardianElderWeekReportDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityGuardianElderWeekReportMealBinding.inflate(layoutInflater)
+        binding = ActivityGuardianElderWeekReportDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // 오늘의 식단 추천
-        binding.btnMealRecommend.setOnClickListener {
+        binding.btnRoutineRecommend.setOnClickListener {
             // 어르신 질병명 입력 다이얼로그 표시
             getDiseaseNameDialog()
         }
